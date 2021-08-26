a54e7850bd928caaa36108476b81d1f4699d9ae4
PetarSimonovic <petar@simonovic.com>
2021-08-26 19:57:56 +0100

    date bugs fixed

--- calendardata.py
+++ calendardata.py
@@ -1,7 +1,8 @@
 import datetime
+from requests import get
 import json
 import pprint
-from requests import get
+import random
 from pymeeus.Sun import Sun
 from pymeeus.Epoch import Epoch
 
@@ -11,13 +12,15 @@ day  = today.strftime("%d")
 month = today.strftime("%m")
 month_name = today.strftime("%B")
 year = today.strftime("%Y")
-day_name = today.strftime('%A')  
+day_name = today.strftime('%A')
 today_date = f'{year}{month}{day}'
+day_data = f'{day_name} {day} {month_name} {year}'
 bank_holidays_API = get('https://www.gov.uk/bank-holidays.json').json()
+
 
 def calculate_solstice(target):
 	year = datetime.datetime.now().year
-	solstice = Sun.get_equinox_solstice(year, target=target) 
+	solstice = Sun.get_equinox_solstice(year, target=target)
 	y, m, d, h, mi, s = solstice.get_full_date(utc=True)
 	solstice_date = (f'{y}{m}{d}')
 	solstice_day = f'{d}'
@@ -26,7 +29,7 @@ def calculate_solstice(target):
 	print(solstice_time)
 	print(today_date)
 	print(day + solstice_day)
-	print(month)	
+	print(month)
 	if solstice_day == day:
 		return solstice_time
 
@@ -44,38 +47,56 @@ def get_solstice():
 
 def get_bank_holiday(today):
 	print(today)
-	if today == f'{year}-10-31}':
+	if today == f'{year}-10-31':
 		return "Halloween"
-	elif today == f'{year}-12-24}':
+	elif today == f'{year}-12-24':
 		return "Christmas Eve"
+	elif today == f'{year}-08-08':
+		return "International Cat Day"
 	bank_holidays = bank_holidays_API.get('england-and-wales')
 	events = bank_holidays.get('events')
 	for event in events:
 		if event['date'] == today:
 			return event['title']
+	return ""
 
 def santa_tracker():
 	return "Santa is in the North Pole"
 
+def get_scare():
+	return random.choice([
+	"Did you hear that?",
+	"There's someone behind you",
+	"Can you feel a ... presence?",
+	"I'm pretty sure I'm haunted",
+	"Something was scratching at the door",
+	"I wouldn't go outside tonight",
+	])
+
+def get_joke():
+	return random.choice([
+	"What is Santa’s favourite kind of pizza? One that’s deep-pan, crisp and even"
+	])
+
 def get_holiday_message(holiday):
 	if holiday == 'Christmas Eve':
-		return santa_tracker
-	if holiday == 'Christmas Day':
-		return 'Merry Christmas'
-	if holiday == 'Halloween'
-		scare = True 
-		return 'Happy Halloween'
-	if holiday == "New Year's Day"
+		return santa_tracker()
+	elif holiday == 'Christmas Day':
+		return f'Merry Christmas {get_joke()}'
+	elif holiday == 'Halloween':
+		return f'Happy Halloween {get_scare()}'
+	elif holiday == "New Year's Day":
 		return f"Happy New Year have a great {year}"
-
+	else:
+		return ""
 
 def fetch():
-	scare = False
-	solstice = get_solstice()
-	holiday = get_bank_holiday(f'{year}-{month}-{day}')
-	holiday_message = get_holiday_message(holiday)
-	day_data = f'{day_name} {day} {month_name} {year}'
-	solstice = get_solstice()
-	calendar_report = f'{day_data} {bank_holiday} {holiday_message} {solstice()} 
-	print(calendar_report)
-	return calendar_report
+        scare = False
+        solstice = get_solstice()
+        holiday = get_bank_holiday(f'{year}-{month}-{day}')
+        holiday_message = get_holiday_message(holiday)
+        day_data = f'{day_name} {day} {month_name} {year}'
+        solstice = get_solstice()
+        calendar_report = f'{day_data} {holiday} {holiday_message} {solstice}'
+        print(calendar_report)
+        return calendar_report
