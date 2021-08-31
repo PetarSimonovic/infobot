404df32fbfb660a5e985e4ec67e020c2b320e064
PetarSimonovic <petar@simonovic.com>
2021-08-31 21:54:03 +0100

    bug fixes

--- infobot.py
+++ infobot.py
@@ -1,4 +1,5 @@
 import schedule
+import datetime
 import calendardata
 import weatherdata
 import scrollphathd as sphd
@@ -12,9 +13,10 @@ year = ""
 day_name = ""
 today_date = ""
 day_data = ""
+day_string = ""
 holiday = ""
 
-def set_date:
+def set_date():
 	print("Setting the date")
 	today = datetime.date.today()
 	day  = today.strftime("%d")
@@ -24,14 +26,14 @@ def set_date:
 	day_name = today.strftime('%A')
 	today_date = f'{year}-{month}-{day}'
 	day_string = f'{day_name} {day} {month_name} {year}'
-	holiday = calendardata.get_bank_holiday(today-date)
+	holiday = calendardata.get_bank_holiday(f'{year}-{month}-{day}')
 
 def create_infostring():
 	sphd.clear()
 	print("Creating infostring")
 	weather_report = weatherdata.fetch()
 	holiday_message = calendardata.get_holiday_message(holiday)
-	infostring = f'{day_string} {get_solstice(month, day)} {holiday} {holiday_message} {calendar_report} {weather_report}'
+	infostring = f'{day_string} {calendardata.get_solstice(month, day)} {holiday} {holiday_message} {weather_report} '
 	print(infostring)
 	display_infostring(infostring)
 
@@ -46,6 +48,7 @@ def display_infostring(infostring):
 		time.sleep(0.03)
 
 def start_infobot():
+	set_date()
 	create_infostring()
 	schedule.every(1).hours.do(create_infostring)
 	schedule.every().day.at("00:00").do(set_date)
