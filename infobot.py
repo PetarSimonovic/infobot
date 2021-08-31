7daba86766c59c6fa2bc6b9ff189a83c7fcc279e
PetarSimonovic <petar@simonovic.com>
2021-08-31 15:06:12 +0100

    create_infostring and display_infostring added

--- infobot.py
+++ infobot.py
@@ -2,20 +2,26 @@ import calendardata
 import weatherdata
 import scrollphathd as sphd
 import time
-
-weather_report = weatherdata.fetch()
-
-calendar_report = calendardata.fetch()
-
-infostring = f'{calendar_report} {weather_report}'
-print(infostring)
+from datetime import datetime, timedelta
 
 
-sphd.write_string(infostring)
-sphd.set_brightness(0.3)
 
-while True:
-	sphd.show()
-	sphd.scroll(1)
-	time.sleep(0.03)
+def create_infostring():
+	weather_report = weatherdata.fetch()
+	calendar_report = calendardata.fetch()
+	infostring = f'{calendar_report} {weather_report}'
+	print(infostring)
+	display_infostring(infostring)
 
+
+def display_infostring(infostring):
+	sphd.clear()
+	sphd.write_string(infostring)
+	sphd.set_brightness(0.3)
+	while True:
+		sphd.show()
+		sphd.scroll(1)
+		time.sleep(0.03)
+
+
+create_infostring()
